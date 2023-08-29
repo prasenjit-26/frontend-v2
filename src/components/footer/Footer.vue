bb7fea0c0ba7e171b85b08f645e872ad9b368e65
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2023-08-29 16:36:23 +0530

    footer ui changes

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -12,7 +12,7 @@ const { networkSlug } = useNetwork();
       class="xl:container lg:px-4 xl:mx-auto dark:border-t dark:border-gray-800"
     >
       <div
-        class="flex flex-col md:flex-row gap-8 md:justify-between py-4 px-4 lg:px-0 text-primary"
+        class="flex flex-col md:flex-row gap-8 md:justify-between items-center py-4 px-4 lg:px-0"
       >
         <div class="flex flex-col gap-8">
           <router-link
@@ -22,9 +22,46 @@ const { networkSlug } = useNetwork();
             <AppLogo class="mb-4" />
           </router-link>
         </div>
-        <div></div>
+        <div class="columns-2 md:columns-2 sm:columns-1">
+          <div class="flex flex-col">
+            <a class="link-text">About</a>
+            <a class="link-text">Docs</a>
+            <a class="link-text">Forum</a>
+            <a class="link-text">Bug Bounty</a>
+          </div>
+          <div class="flex flex-col">
+            <a class="link-text">Terms of Use</a>
+            <a class="link-text">Privacy Policy</a>
+            <a class="link-text">Cookie Policy</a>
+            <a class="link-text">3rd Party services</a>
+          </div>
+        </div>
       </div>
       <div class="border-bottom" />
+      <div class="flex flex-col md:flex-row gap-8 md:justify-between pb-[40px]">
+        <span class="copyright-text">
+          ©2023 Chimpexchange. All rights reserved.
+        </span>
+        <div class="flex gap-3 justify-center items-center">
+          <a>
+            <img
+              src="~@/assets/images/Twitter.png"
+              alt="twitter"
+              width="40px"
+            />
+          </a>
+          <a>
+            <img
+              src="~@/assets/images/Linkedin.png"
+              alt="twitter"
+              width="40px"
+            />
+          </a>
+          <a>
+            <img src="~@/assets/images/Gmail.png" alt="twitter" width="40px" />
+          </a>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -83,5 +120,15 @@ footer :deep(.logotype) {
 
 .router-link-active {
   @apply text-blue-600 dark:text-blue-400;
+}
+.link-text {
+  font-size: 16px;
+  font-weight: 600;
+  padding: 16px 20px;
+}
+.copyright-text {
+  color: #d1d1d1;
+  font-size: 18px;
+  font-weight: 500;
 }
 </style>
