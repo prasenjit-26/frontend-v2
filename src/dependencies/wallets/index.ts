5802bf56d8f055dffc14bef7c4116f59bdd0a5df
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2024-10-24 20:02:16 +0530

    init wallet connector

--- src/dependencies/wallets/index.ts
+++ src/dependencies/wallets/index.ts
@@ -1,8 +1,10 @@
 import { initSafeConnector } from './safe';
 import { initMetamaskConnector } from './metamask';
+import { initFoxWalletConnector } from './foxwallet';
 import { initTallyConnector } from './tally';
 import { initWalletconnectConnector } from './walletconnect';
 import { initWalletLinkConnector } from './walletlink';
+import { initEchoooEthConnector } from './echoooEth';
 
 export function initWalletConnectors() {
   initWalletconnectConnector();
@@ -10,4 +12,6 @@ export function initWalletConnectors() {
   initSafeConnector();
   initWalletLinkConnector();
   initTallyConnector();
+  initFoxWalletConnector();
+  initEchoooEthConnector();
 }
